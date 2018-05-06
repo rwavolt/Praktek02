3ed8e107bef341bd0ba0faf0fc69ea66e7dcf66d
Rama Indrawan <rama.indrawan@yahoo.com>
2018-05-06 21:11:02 +0800

    Membuat Attribute Dan Behavior

--- src/praktek02/Pyramid.java
+++ src/praktek02/Pyramid.java
@@ -1,4 +1,15 @@
 package praktek02;
 public class Pyramid {
+    double palas;
+    double lalas;
+    double tinggi;
+    
+    void cetakInfo(){
+        System.out.println("======================");
+        System.out.println("Panjang Alas : "+palas);
+        System.out.println("Lebar Alas   : "+lalas);
+        System.out.println("Tinggi       : "+tinggi);
+        System.out.println("======================");
+    }
     
 }
