48dbcb10611aad67743a39f5e2f4624207c0692d
Rama Indrawan <rama.indrawan@yahoo.com>
2018-05-06 21:21:35 +0800

    Mengisi Nilai Attribute

--- src/praktek02/PyramidAksi.java
+++ src/praktek02/PyramidAksi.java
@@ -3,5 +3,8 @@ public class PyramidAksi {
     public static void main(String[] args) {
         
         Pyramid p1 = new Pyramid();
+        p1.palas = 6;
+        p1.lalas = 5;
+        p1.tinggi = 7;
     }
 }
